3f74ca35a5d69d615fcf8f3176baf54923e4b38e
Tian <zaqttan@gmail.com>
2016-01-04 14:24:11 +0800

    nav standalone add on

--- src/main.js
+++
@@ -1,9 +0,0 @@
-var navStandAlone = function standalone () {
-  if (window.navigator.standalone === true) {
-    // return 'NAVIGATOR IS WORKING!!!'
-    document.querySelector('body').textContent = 'WORKING!!'
-  } else {
-    document.querySelector('body').textContent = 'NOT WORKING!!'
-  }
-}
-navStandAlone()
